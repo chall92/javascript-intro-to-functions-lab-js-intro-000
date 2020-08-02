185167f4b0affc4ef73b23bcce6ca103b83130d6
Corbin Hall <hallcorbin92@gmail.com>
2020-08-02 01:27:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return 'I love you, too.'
   }
 
-  else if(string === uppercase) {
+  else if(string === string.toLowerCase()) {
     return 'YES INDEED!'
   }
   else if(string === lowercase) {
